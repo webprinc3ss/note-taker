97eb90d5329b620aa00a2e253d0837c8aba8da42
Mystic Kitty <djabranton@gmail.com>
2020-11-27 11:49:36 -0800

    Added html routes.

---
+++ server.js
@@ -0,0 +1,17 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming JSON data
+app.use(express.json());
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
