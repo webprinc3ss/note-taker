735f732570fc9368944dfb5b66c49fde5c9ddde8
Mystic Kitty <djabranton@gmail.com>
2020-11-28 00:01:00 -0800

    Created delete function.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,6 +8,4 @@ module.exports = function (app) {
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
-
-
 }
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+const fs = require("fs");
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// user public assets
 app.use(express.static('public'));
 // parse incoming JSON data
 app.use(express.json());
 
+// connect to routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+
+//Are you listening?
 app.listen(PORT, () => console.log(`API server now on port ${PORT}!`));
